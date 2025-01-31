9c66304001351d0ef94a7050a0897290a0367bd9
Giuseppe Viscoli <gviscolio@hotmail.com>
2025-01-31 22:40:50 +0100

    .

---
+++ 03.BackToTheFuture/pch-back2future.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include "E:\SVILUPPO\APPLE 2\00.LIBRERIE\CC65\inc\apple2.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../00.LIBRERIE/IP65/inc/ip65.h"
+
+// https://github.com/oliverschmidt/stream-pdm/blob/main/w5100_http.c
+
+// https://github-wiki-see.page/m/cc65/ip65/wiki/Library-for-C-programs
+
+// https://github-wiki-see.page/m/cc65/ip65/wiki/Tweet65
+
+
+// Compilazione
+//
+// cl65 -t apple2 pch-back2future.c -o pch-back2future.bin -O  -m pch-back2future.map -vm ../00.LIBRERIE/IP65/lib/ip65.lib ../00.LIBRERIE/IP65/drivers/ip65_apple2.lib
+//
+
+int main() {
+    
+    return 0;
+}
