56b7079d9030353a3769ff84c366ce05cd21e10e
Giuseppe Viscoli <gviscolio@hotmail.com>
2025-02-08 23:26:03 +0100

    .

--- 03.BackToTheFuture/test-client.c
+++ 03.BackToTheFuture/test-client.c
@@ -2,49 +2,70 @@
 
 // Compilazione
 //
-// cl65 -t apple2 test-client.c stream3.c -o test-client.bin -O  -m test-client.map -vm ../00.LIBRERIE/IP65/lib/ip65.lib ../00.LIBRERIE/IP65/drivers/ip65_apple2.lib
-//
-// cl65 -t apple2 test-client.c net-utility.s -o test-client.bin -O  -m test-client.map -vm ../00.LIBRERIE/IP65/lib/ip65.lib ../00.LIBRERIE/IP65/drivers/ip65_apple2.lib
-//
-// ca65 net-utility.s -o net-utility.o
-// cc65 -O -t apple2 test-client.c --add-source
-// ca65 test-client.s -o test-client.o
-// ld65 -t apple2 -o test-client.bin test-client.o net-utility.o ../00.LIBRERIE/IP65/lib/ip65.lib ../00.LIBRERIE/IP65/drivers/ip65_apple2.lib
+// cl65 -t apple2 test-client.c -o test-client.bin -O  -m test-client.map -vm ../00.LIBRERIE/IP65/lib/ip65.lib ../00.LIBRERIE/IP65/lib/ip65_tcp.lib ../00.LIBRERIE/IP65/drivers/ip65_apple2.lib ../00.LIBRERIE/CC65/lib/apple2.lib
 //
 // java -jar C:\UTILITY\AppleCommander\AppleCommander-win32-x86_64-1.9.0.jar
 // 
 //
 
 #include <stdio.h>
+#include <string.h>
+
 #include "../00.LIBRERIE/IP65/inc/ip65.h"
 
 
-// Dichiarazione della funzione Assembly
-extern void init_ip_via_dhcp(void);
+#define SERVER "google.com"  // Cambia con il dominio del server
+#define PORT   80             // Porta HTTP
+#define BUFFER_SIZE 512
 
-// Funzione per stampare l'indirizzo IP attuale
-void print_ip_address(void) {
-    extern unsigned char cfg_ip[4];  // Indirizzo IP assegnato dal DHCP
+unsigned char buffer[BUFFER_SIZE];  // Buffer per i dati ricevuti
 
-    printf("Indirizzo IP assegnato: %d.%d.%d.%d\n",
-           cfg_ip[0], cfg_ip[1], cfg_ip[2], cfg_ip[3]);
-}
+void main() 
+{
+    uint32_t server_ip;
+    unsigned char ip_bytes[4];
+    char http_request[] = "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n";
+    int received;
 
-int main(void) {
-    printf("Inizializzazione della rete via DHCP...\n");
-    init_ip_via_dhcp();  // Chiama la funzione Assembly
-    printf("Operazione completata.\n");
+    ip65_init(0);  // Inizializza la rete
 
-    print_ip_address();  // Stampa l'IP assegnato
+    printf("Avvio DHCP...\n");
+    if (dhcp_init() != 0) {
+        printf("Errore DHCP!\n");
+        return;
+    }
 
-    // printf("Inizializzazione della rete via DHCP...\n");
+    printf("Risoluzione DNS per %s...\n", SERVER);
+    
+    if (dns_resolve(SERVER) != 0) {
+        printf("Errore DNS!\n");
+        return;
+    }
 
-    // if (init_ip_via_dhcp() == 0) {
-    //     printf("Rete configurata con successo!\n");
-    //     print_ip_address();  // Stampa l'IP assegnato
-    // } else {
-    //     printf("Errore nella configurazione della rete.\n");
+    // unsigned char ip_bytes[4];
+    // memcpy(ip_bytes, &server_ip, 4);
+    // printf("Indirizzo IP: %d.%d.%d.%d\n", ip_bytes[0], ip_bytes[1], ip_bytes[2], ip_bytes[3]);
+
+    // printf("Connessione a %s...\n", SERVER);
+    // if (ip65_tcp_connect(server_ip, PORT) != 0) {
+    //     printf("Errore di connessione!\n");
+    //     return;
     // }
 
-    return 0;
+    // printf("Invio richiesta HTTP...\n");
+    // char http_request[] = "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n";
+    // if (ip65_tcp_send((unsigned char*)http_request, strlen(http_request)) != 0) {
+    //     printf("Errore invio dati!\n");
+    //     return;
+    // }
+
+    // printf("Ricezione dati...\n");
+    // int received;
+    // while ((received = ip65_tcp_recv(buffer, BUFFER_SIZE)) > 0) {
+    //     buffer[received] = '\0';  // Assicura che il buffer sia una stringa valida
+    //     printf("%s", buffer);     // Stampa la risposta HTTP
+    // }
+
+    printf("\nChiusura connessione.\n");
+    tcp_close();
 }
